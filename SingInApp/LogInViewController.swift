5dd6392816b2556bd241cd42892e0aaaff65a8ac
immortalprogrammist <immortalvelial@icloud.com>
2021-07-12 23:33:39 +0200

    worked with main.storyboard, LogInVC, ProfileVC

--- SingInApp/LogInViewController.swift
+++ SingInApp/LogInViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var PasswordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let profileVC = segue.destination as? ProfileViewController else { return }
+
+        profileVC.userName = userNameTF.text
+    }
+    
+    @IBAction func showForgotUserName() {
+    }
+    
+    @IBAction func showForgotPassword() {
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        PasswordTF.text = ""
+        dismiss(animated: true)
+    }
+    
 }
