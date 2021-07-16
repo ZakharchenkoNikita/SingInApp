2b472ee4c036d54bf62d014f79f6d10219311184
immortalprogrammist <immortalvelial@icloud.com>
2021-07-16 10:58:18 +0200

    start with MVC pattern created User.swift, Profile.swift

---
+++ SingInApp/Models/Profile.swift
@@ -0,0 +1,12 @@
+//
+//  Profile.swift
+//  SingInApp
+//
+//  Created by Nikita on 16.07.21.
+//
+
+struct Profile {
+    let name: String
+    let surmane: String
+    let age: Int
+}
---
+++ SingInApp/Models/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  SingInApp
+//
+//  Created by Nikita on 16.07.21.
+//
+
+struct User {
+    let login: String
+    let password: String
+    let profile: Profile
+}
