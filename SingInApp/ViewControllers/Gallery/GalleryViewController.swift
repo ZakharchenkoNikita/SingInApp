a78e516d2b163e705ef4e68ee42ac65fb9f2bcda
immortalprogrammist <immortalvelial@icloud.com>
2021-07-18 19:47:23 +0200

    done

--- SingInApp/ViewControllers/Gallery/GalleryViewController.swift
+++ SingInApp/ViewControllers/Gallery/GalleryViewController.swift
@@ -18,6 +18,7 @@ class GalleryViewController: UIViewController {
     // MARK: override methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         collectionView.delegate = self
         collectionView.dataSource = self
     }
