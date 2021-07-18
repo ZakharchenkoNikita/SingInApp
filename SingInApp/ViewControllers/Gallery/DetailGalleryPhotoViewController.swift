6b8e092f25a367e0fbbaa74cde2dd53a233d192c
immortalprogrammist <immortalvelial@icloud.com>
2021-07-18 19:15:58 +0200

    -

--- SingInApp/ViewControllers/Gallery/DetailGalleryPhotoViewController.swift
+++ SingInApp/ViewControllers/Gallery/DetailGalleryPhotoViewController.swift
@@ -18,6 +18,7 @@ class DetailGalleryPhotoViewController: UIViewController {
     // MARK: override methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         detailGalleryPhoto.image = image
     }
 }
